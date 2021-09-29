f5e049a20ecf6200d9683fc0557b086e8394c3b5
Subhadip <89024718+Subhadiptech@users.noreply.github.com>
2021-09-29 12:43:22 +0530

    updated

--- app/src/main/java/com/ersubhadip/hackathon/fragment_book.java
+++ app/src/main/java/com/ersubhadip/hackathon/fragment_book.java
@@ -27,12 +27,15 @@ public class fragment_book extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book, container, false);
+        View view = inflater.inflate(R.layout.fragment_book, container, false);
+          //initialisation
+        return view;
     }
      @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         
         super.onViewCreated(view, savedInstanceState);
+       //operational statements
         
     }
 }
