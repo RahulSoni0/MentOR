548904e8ca2f417222caef7d43953ee5261e54b3
RahulSoni0 <75351409+RahulSoni0@users.noreply.github.com>
2021-09-29 08:54:17 +0530

    v_0_0_1

--- app/src/main/java/com/ersubhadip/hackathon/MainActivity.java
+++ app/src/main/java/com/ersubhadip/hackathon/MainActivity.java
@@ -1,14 +1,70 @@
 package com.ersubhadip.hackathon;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import nl.joery.animatedbottombar.AnimatedBottomBar;
 
 public class MainActivity extends AppCompatActivity {
-
+    AnimatedBottomBar animatedBottomBar;
+    FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        animatedBottomBar = findViewById(R.id.animatedBottomBar);
+
+        if (savedInstanceState == null) {
+            animatedBottomBar.selectTabById(R.id.home, true);
+            fragmentManager = getSupportFragmentManager();
+            fragment_home homeFragment = new fragment_home();
+            fragmentManager.beginTransaction().replace(R.id.fragment_container, homeFragment)
+                    .commit();
+        }
+
+
+        animatedBottomBar.setOnTabSelectListener(new AnimatedBottomBar.OnTabSelectListener() {
+            @Override
+            public void onTabSelected(int lastIndex, @Nullable AnimatedBottomBar.Tab lastTab, int newIndex, @NotNull AnimatedBottomBar.Tab newTab) {
+                Fragment fragment = null;
+                switch (newTab.getId()) {
+                    case R.id.home:
+                        fragment = new fragment_home();
+                        break;
+                    case R.id.book:
+                        fragment = new fragment_book();
+                        break;
+                    case R.id.account:
+                        fragment = new fragment_account();
+                        break;
+                }
+
+                if (fragment != null) {
+                    fragmentManager = getSupportFragmentManager();
+                    fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment)
+                            .commit();
+                } else {
+
+                }
+            }
+        });
+
+
+
+
+
+
+
+
+
+
     }
 }
