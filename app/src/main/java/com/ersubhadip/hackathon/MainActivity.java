29a3933ac0a2ec659ab666d6e2e120c226718092
RahulSoni0 <rahul.kumar.soni2511@gmail.com>
2021-09-29 13:04:59 +0530

    v.0.0.3

--- app/src/main/java/com/ersubhadip/hackathon/MainActivity.java
+++ app/src/main/java/com/ersubhadip/hackathon/MainActivity.java
@@ -1,14 +1,55 @@
 package com.ersubhadip.hackathon;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import nl.joery.animatedbottombar.AnimatedBottomBar;
+
+public class MainActivity extends AppCompatActivity {
+    AnimatedBottomBar animatedBottomBar;
+    FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        animatedBottomBar = findViewById(R.id.animatedBottomBar);
+
+
+        animatedBottomBar.setOnTabSelectListener(new AnimatedBottomBar.OnTabSelectListener() {
+            @Override
+            public void onTabSelected(int lastIndex, @Nullable AnimatedBottomBar.Tab lastTab, int newIndex, @NotNull AnimatedBottomBar.Tab newTab) {
+                Fragment fragment = null;
+                switch (newTab.getId()) {
+                    case R.id.home:
+                        fragment = new fragment_home();
+                        break;
+                    case R.id.book:
+                        fragment = new fragment_book();
+                        break;
+                    case R.id.account:
+                        fragment = new fragment_account();
+                        break;
+                }
+
+                if (fragment != null) {
+                    fragmentManager = getSupportFragmentManager();
+                    fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment)
+                            .commit();
+                } else {
+
+                }
+            }
+        });
+
+
+
+
     }
 }
--- app/src/main/java/com/ersubhadip/hackathon/fragment_book.java
+++ app/src/main/java/com/ersubhadip/hackathon/fragment_book.java
@@ -2,11 +2,14 @@ package com.ersubhadip.hackathon;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import org.jetbrains.annotations.Nullable;
 
 public class fragment_book extends Fragment {
 
