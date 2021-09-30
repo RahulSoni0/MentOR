b386c3e960b4e0f9eead2978979b4fddaa588756
Subhadiptech <2020ugee055@nitjsr.ac.in>
2021-09-30 17:37:44 +0530

    splash added

--- app/src/main/java/com/ersubhadip/hackathon/Activity/SplashActivity.java
+++ app/src/main/java/com/ersubhadip/hackathon/Activity/SplashActivity.java
@@ -1,18 +1,30 @@
 package com.ersubhadip.hackathon.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
 import com.ersubhadip.hackathon.R;
 
 public class SplashActivity extends AppCompatActivity {
+    private AppCompatButton signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        signUp=findViewById(R.id.SplashSignUpBtn);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
 
     }
 }
