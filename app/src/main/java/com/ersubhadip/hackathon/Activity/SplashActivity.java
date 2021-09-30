cd36ad1cfb0ffa8873fa020605cbf9cfcfbf1867
Subhadiptech <2020ugee055@nitjsr.ac.in>
2021-10-01 00:00:51 +0530

    splash java code added

--- app/src/main/java/com/ersubhadip/hackathon/Activity/SplashActivity.java
+++ app/src/main/java/com/ersubhadip/hackathon/Activity/SplashActivity.java
@@ -3,14 +3,17 @@ package com.ersubhadip.hackathon.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
 
 import com.ersubhadip.hackathon.R;
 
 public class SplashActivity extends AppCompatActivity {
     private AppCompatButton signUp;
+    private TextView login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,24 @@ public class SplashActivity extends AppCompatActivity {
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                Intent it=new Intent(SplashActivity.this,SignUpActivity.class);
+                startActivity(it);
+                finish();
+
+
+
+            }
+        });
+
+        //todo:status bar color and physical key functions
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it=new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(it);
+                finish();
 
 
 
