dc52ec432fa12109705c121397b07974714704e4
Subhadiptech <2020ugee055@nitjsr.ac.in>
2021-10-15 21:29:35 +0530

    style added to tabActionbar and back button implemented

--- app/src/main/java/com/ersubhadip/hackathon/Activity/TabedActivity.java
+++ app/src/main/java/com/ersubhadip/hackathon/Activity/TabedActivity.java
@@ -1,17 +1,46 @@
 package com.ersubhadip.hackathon.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.ersubhadip.hackathon.R;
+import com.google.android.material.tabs.TabLayout;
 
 public class TabedActivity extends AppCompatActivity {
+
+    private TabLayout tab;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabed);
+        tab=findViewById(R.id.tabMain);
 
+        //setting Action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setTitle("Open Source");
+
+        //end
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==android.R.id.home){
+
+            finish();
+            return  true;
+
+        }else{
+
+            return false;
+
+        }
     }
 }
--- app/src/main/java/com/ersubhadip/hackathon/Classes/booksRvAdapter.java
+++ app/src/main/java/com/ersubhadip/hackathon/Classes/booksRvAdapter.java
@@ -43,19 +43,7 @@ public class booksRvAdapter  extends RecyclerView.Adapter<booksRvAdapter.ViewHol
 
         //setting colors list to fetch random colors
         ArrayList<String> colors=new ArrayList<>();
-        colors.add("#48546E");
-        colors.add("#662EC3");
-        colors.add("#FC407D");
-        colors.add("#4185F4");
-        colors.add("#A1DD70");
-        colors.add("#A3D0EB");
-        colors.add("#FB7F64");
-        colors.add("#ADDCCA");
-        colors.add("#7DC9E7");
-        colors.add("#EFADE4");
-        colors.add("#13C0E5");
-        colors.add("#DE1B85");
-        colors.add("#F6404F");
+        colors.add("#48546E");colors.add("#662EC3");colors.add("#FC407D");colors.add("#4185F4");colors.add("#A1DD70");colors.add("#A3D0EB");colors.add("#FB7F64");colors.add("#ADDCCA");colors.add("#7DC9E7");colors.add("#EFADE4");colors.add("#13C0E5");colors.add("#DE1B85");colors.add("#F6404F");
         //end
 
         //Random Class Calling
