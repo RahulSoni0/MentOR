022a840fd7b5ac753b8874d039a2e0b2d521a06c
Subhadiptech <2020ugee055@nitjsr.ac.in>
2021-10-18 04:35:39 +0530

    basic poster slider done from scratch - for testing, no library used

--- app/src/main/java/com/ersubhadip/hackathon/Classes/homeAdapter.java
+++ app/src/main/java/com/ersubhadip/hackathon/Classes/homeAdapter.java
@@ -1,5 +1,6 @@
 package com.ersubhadip.hackathon.Classes;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.ersubhadip.hackathon.Activity.TabedActivity;
 import com.ersubhadip.hackathon.R;
 
 import java.util.ArrayList;
@@ -45,13 +47,30 @@ public class homeAdapter  extends RecyclerView.Adapter<homeAdapter.ViewHolder>{
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             homeTitle=itemView.findViewById(R.id.homeCoursetitle);
+
+
+
         }
 
         private void setData(String s){
 
             homeTitle.setText(s);
 
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    //todo:send essential data
+                    Intent it=new Intent(itemView.getContext(), TabedActivity.class);
+                    itemView.getContext().startActivity(it);
+
+                }
+            });
+
         }
+
+
 
     }
 }
